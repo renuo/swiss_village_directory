074e0ac77c3b5241b8cd34a21ce38d00a2dc4a4e
Simon Huber <simon.huber@renuo.ch>
2016-07-06 14:58:02 +0200

    convert values to numeric

--- lib/swiss_village_directory/village.rb
+++ lib/swiss_village_directory/village.rb
@@ -1,15 +1,15 @@
 module SwissVillageDirectory
   class Village
-    attr_reader :name, :zip_code, :commune, :canton, :latitude, :longitude
+    attr_reader :name, :zip_code, :one_digit_spare, :commune, :canton, :longitude, :latitude
 
-    def initialize(name, zip_code, commune, canton, latitude, longitude)
+    def initialize(name, zip_code, one_digit_spare, commune, canton, longitude, latitude)
       @name = name
-      @zip_code = zip_code
+      @zip_code = zip_code.to_i
       @one_digit_spare = one_digit_spare.to_i
       @commune = commune
       @canton = canton
-      @latitude = latitude
-      @longitude = longitude
+      @longitude = longitude.to_f
+      @latitude = latitude.to_f
     end
   end
 end
--- spec/swiss_village_directory/village_spec.rb
+++ spec/swiss_village_directory/village_spec.rb
@@ -10,4 +10,15 @@ describe SwissVillageDirectory::Village do
   it { is_expected.to respond_to(:canton) }
   it { is_expected.to respond_to(:latitude) }
   it { is_expected.to respond_to(:longitude) }
+
+  it 'has a numeric values' do
+    expect_is_numeric(subject.zip_code)
+    expect_is_numeric(subject.one_digit_spare)
+    expect_is_numeric(subject.latitude)
+    expect_is_numeric(subject.longitude)
+  end
+
+  def expect_is_numeric(value)
+    expect(value).to be_a Numeric
+  end
 end
