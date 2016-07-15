dd42339c2b86860317bfa164c5a0a5f0d73d6379
Simon Huber <simon.huber@renuo.ch>
2016-07-15 18:41:12 +0200

    refactor

--- lib/swiss_village_directory/repository.rb
+++ lib/swiss_village_directory/repository.rb
@@ -12,22 +12,20 @@ module SwissVillageDirectory
       end
     end
 
-    def find(find_params = {})
-      @villages.find_all { |v| filter_matched?(v, find_params) }
+    def find_all_by(find_params)
+      @villages.find_all { |v| all_filters_matched?(v, find_params) }
     end
 
     private
 
-    def filter_matched?(village, filter_params)
-      match = true
+    def all_filters_matched?(village, filter_params)
       filter_params.each do |p|
-        begin
-          match = false if village.send(p.first) != p.last
-        rescue
-          match = false
-        end
+        return false if filter_matches?(village, p.first, p.last)
       end
-      match
+    end
+
+    def filter_matches?(village, key, value)
+      village.instance_variable_get("@#{key}") != value
     end
   end
 end
--- spec/swiss_village_directory/repository_spec.rb
+++ spec/swiss_village_directory/repository_spec.rb
@@ -36,30 +36,30 @@ describe SwissVillageDirectory::Repository do
     end
 
     it 'finds by attribute' do
-      expect(repo.find(name: 'Aadorf_wrong').count).to eq(0)
-      expect(repo.find(name: 'Aadorf').count).to eq(1)
-      expect(repo.find(zip_code: 0).count).to eq(0)
-      expect(repo.find(zip_code: 8355).count).to eq(1)
-      expect(repo.find(one_digit_spare: 1).count).to eq(0)
-      expect(repo.find(one_digit_spare: 0).count).to eq(2)
-      expect(repo.find(commune: 'AadorfCommune_wrong').count).to eq(0)
-      expect(repo.find(commune: 'AadorfCommune').count).to eq(2)
-      expect(repo.find(canton: 'TG_wrong').count).to eq(0)
-      expect(repo.find(canton: 'TG').count).to eq(1)
-      expect(repo.find(longitude: 0).count).to eq(0)
-      expect(repo.find(longitude: 8.089).count).to eq(0)
-      expect(repo.find(longitude: 8.906).count).to eq(1)
-      expect(repo.find(latitude: 47.099).count).to eq(0)
-      expect(repo.find(latitude: 47.496).count).to eq(1)
+      expect(repo.find_all_by(name: 'Aadorf_wrong').count).to eq(0)
+      expect(repo.find_all_by(name: 'Aadorf').count).to eq(1)
+      expect(repo.find_all_by(zip_code: 0).count).to eq(0)
+      expect(repo.find_all_by(zip_code: 8355).count).to eq(1)
+      expect(repo.find_all_by(one_digit_spare: 1).count).to eq(0)
+      expect(repo.find_all_by(one_digit_spare: 0).count).to eq(2)
+      expect(repo.find_all_by(commune: 'AadorfCommune_wrong').count).to eq(0)
+      expect(repo.find_all_by(commune: 'AadorfCommune').count).to eq(2)
+      expect(repo.find_all_by(canton: 'TG_wrong').count).to eq(0)
+      expect(repo.find_all_by(canton: 'TG').count).to eq(1)
+      expect(repo.find_all_by(longitude: 0).count).to eq(0)
+      expect(repo.find_all_by(longitude: 8.089).count).to eq(0)
+      expect(repo.find_all_by(longitude: 8.906).count).to eq(1)
+      expect(repo.find_all_by(latitude: 47.099).count).to eq(0)
+      expect(repo.find_all_by(latitude: 47.496).count).to eq(1)
     end
 
     it 'works if a random attribute is returned' do
-      expect(repo.find(random_attribute: 'random').count).to eq(0)
+      expect(repo.find_all_by(random_attribute: 'random').count).to eq(0)
     end
 
     it 'finds by using multiple attributes' do
-      expect(repo.find(commune: 'AadorfCommune', canton: 'TG').count).to eq(1)
-      expect(repo.find(commune: 'AadorfCommune', canton: 'ZG').count).to eq(0)
+      expect(repo.find_all_by(commune: 'AadorfCommune', canton: 'TG').count).to eq(1)
+      expect(repo.find_all_by(commune: 'AadorfCommune', canton: 'ZG').count).to eq(0)
     end
   end
 end
