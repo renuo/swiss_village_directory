f00bd4e3978a59f42956371df39d3526e8a603ea
Simon Huber <simon.huber@renuo.ch>
2016-07-19 16:32:11 +0200

    thx rubocop

--- spec/swiss_village_directory/repository_spec.rb
+++ spec/swiss_village_directory/repository_spec.rb
@@ -13,8 +13,8 @@ describe SwissVillageDirectory::Repository do
   end
 
   it 'reads the values from an exel and shows the right ones' do
-    fake_csv = [["Ortschaftsname", "PLZ", "Zusatzziffer", "Gemeindename", "Kantonskürzel", "E", "N"],
-                  ["Aadorf", "8355", "0", "Aadorf", "TG", "8.906", "47.496"]]
+    fake_csv = [%w(Ortschaftsname PLZ Zusatzziffer Gemeindename Kantonskürzel E N),
+                ['Aadorf', '8355', '0', 'Aadorf', 'TG', '8.906', '47.496']]
     expect_any_instance_of(CSV).to receive(:read).and_return(fake_csv)
     expect(repo.villages.count).to eq(1)
     village = repo.villages.find { |v| v.name == 'Aadorf' }
